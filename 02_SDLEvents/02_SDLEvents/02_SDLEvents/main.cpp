6daa44ab242cbb1c02b112202c100472d363d80b
Daniel <zsemberi.daniel@gmail.com>
2020-09-15 13:59:14 +0200

    2. gyakorlat

--- 02_SDLEvents/02_SDLEvents/02_SDLEvents/main.cpp
+++ 02_SDLEvents/02_SDLEvents/02_SDLEvents/main.cpp
@@ -68,6 +68,14 @@ int main( int argc, char* args[] )
 	// egér X és Y koordinátái
 	Sint32 mouseX = 0, mouseY = 0;
 
+	SDL_Rect movinRect;
+	movinRect.x = 0;
+	movinRect.y = 0;
+	movinRect.w = 20;
+	movinRect.h = 20;
+
+	SDL_Color color = {255, 0, 0, 255};
+
 	while (!quit)
 	{
 		// amíg van feldolgozandó üzenet dolgozzuk fel mindet:
@@ -90,6 +98,13 @@ int main( int argc, char* args[] )
 				// egérgomb felengedésének eseménye; a felengedett gomb a ev.button.button -ban található
 				// a lehetséges gombok a következõek: SDL_BUTTON_LEFT, SDL_BUTTON_MIDDLE, 
 				//		SDL_BUTTON_RIGHT, SDL_BUTTON_WHEELUP, SDL_BUTTON_WHEELDOWN
+				// 2. feladat: ha a user a bal egérgombot nyomja meg akkor a téglalap színe váltson pirosra,
+				//    ha a jobb egérgombot, akkor kékre
+				if (ev.button.button == SDL_BUTTON_LEFT) {
+					color = {255, 0, 0, 255};
+				} else if (ev.button.button == SDL_BUTTON_RIGHT) {
+					color = { 0, 0, 255, 255 };
+				}
 				break;
 			}
 		}
@@ -100,40 +115,99 @@ int main( int argc, char* args[] )
 
 		// aktuális rajzolási szín legyen zöld és rajzoljunk ki egy vonalat
 		SDL_SetRenderDrawColor(	ren,	// renderer címe, aminek a rajzolási színét be akarjuk állítani
-								0,		// piros
-								255,	// zöld
-								0,		// kék
-								255);	// átlátszatlanság
+			color.r,		// piros
+			color.g,	// zöld
+			color.b,		// kék
+			255);	// átlátszatlanság
 
 		SDL_RenderDrawLine(	ren,	// renderer címe, ahová vonalat akarunk rajzolni
 							0, 0, // vonal kezdõpontjának (x,y) koordinátái
 							mouseX, mouseY);// vonal végpontjának (x,y) koordinátái
-		
-		// definiáljunk egy (mouseX, mouseY) középpontó, tengelyekkel párhuzamos oldalú
-		// 20x20-as négyzetet:
-		SDL_Rect cursor_rect;
-		cursor_rect.x = mouseX - 10;
-		cursor_rect.y = mouseY - 10;
-		cursor_rect.w = 20;
-		cursor_rect.h = 20;
-		// legyen a kitöltési szín piros
-		SDL_SetRenderDrawColor( ren, 255, 0, 0, 255 );
-		SDL_RenderFillRect( ren, &cursor_rect);
 
 		// 1. feladat: az eltelt idõ függvényében periodikusan nõjjön és csökkenjen
 		//    az egérmutató középpontjával kirajzolt négyszög
+		// sajat feladat:  szelesseg, 10 -> 30, 3mp alatt
+		float time = SDL_GetTicks() / 1000.0f;
+		float width = abs(20.0f * cosf(time / 6.0 * M_PI)) + 10;
 
-		// 2. feladat: ha a user a bal egérgombot nyomja meg akkor a téglalap színe váltson pirosra,
-		//    ha a jobb egérgombot, akkor kékre
+		// 20x20-as négyzetet:
+		SDL_Rect cursor_rect;
+		cursor_rect.x = mouseX - width / 2;
+		cursor_rect.y = mouseY - 10;
+		cursor_rect.w = width;
+		cursor_rect.h = 20;
+		// legyen a kitöltési szín piros
+		SDL_SetRenderDrawColor( ren, color.r, color.g, color.b, 255 );
+		SDL_RenderFillRect( ren, &cursor_rect);
+
+		// 0. feladat: mozogjon egy negyzet jobbra 2 mp-et, fel 2 mp-et es aztan alljon meg
+		int timeTicks = SDL_GetTicks();
+		if (timeTicks < 2000) {
+			movinRect.x += 1;
+		}
+		else if (timeTicks < 4000) {
+			movinRect.y += 1;
+		}
+
+		// legyen a kitöltési szín piros
+		SDL_SetRenderDrawColor(ren, 255, 0, 0, 255);
+		SDL_RenderFillRect(ren, &movinRect);
 
 		// 3. feladat: rajzolj ki egy 50 sugarú körvonalat az egérmutató köré!
 		// segítség: használd a SDL_RenderDrawLines()-t
+		const int N = 80;
+		float radius = 50.0f;
+		SDL_Point points[N + 1];
+		for (int i = 0; i <= N; i++) {
+			float rad = i * 2.0f * M_PI / N;
+			points[i].x = radius * cosf(rad) + mouseX;
+			points[i].y = radius * sinf(rad) + mouseY;
+		}
+
+		SDL_RenderDrawLines(ren, points, N + 1);
+		
+		// projektalt kocka
+		int windowWidth = 640;
+		int windowHeight = 480;
+		int cubeSize = 200;
+		float offset = 30.0;
+
+		// [0;1] - 0.5 => [-.5;0.5] * 2.0 => [-1;1]
+		SDL_Point offsetCoord = {
+			offset * 2.0f * (mouseX / (float) windowWidth - 0.5f), offset * 2.0f * (mouseY / (float) windowHeight - 0.5f)
+		};
+
+		SDL_Point middle = {
+			windowWidth / 2,
+			windowHeight / 2
+		};
+
+		SDL_Rect rect1;
+		rect1.x = middle.x - cubeSize / 2 + offsetCoord.x;
+		rect1.y = middle.y - cubeSize / 2 + offsetCoord.y;
+		rect1.w = cubeSize;
+		rect1.h = cubeSize;
+
+		SDL_SetRenderDrawColor(ren, 255, 0, 0, 255);
+		SDL_RenderDrawRect(ren, &rect1);
+
+
+		SDL_Rect rect2;
+		rect2.x = middle.x - cubeSize / 2 - offsetCoord.x;
+		rect2.y = middle.y - cubeSize / 2 - offsetCoord.y;
+		rect2.w = cubeSize;
+		rect2.h = cubeSize;
+
+		SDL_RenderDrawRect(ren, &rect2);
+		SDL_RenderDrawLine(ren, rect1.x, rect1.y, rect2.x, rect2.y);
+		SDL_RenderDrawLine(ren, rect1.x + rect1.w, rect1.y, rect2.x + rect2.w, rect2.y);
+		SDL_RenderDrawLine(ren, rect1.x, rect1.y + rect1.h, rect2.x, rect2.y + rect2.h);
+		SDL_RenderDrawLine(ren, rect1.x + rect1.w, rect1.y + rect1.h, rect2.x + rect2.w, rect2.y + rect2.h);
+		
 
 		// jelenítsük meg a backbuffer tartalmát
 		SDL_RenderPresent(ren);
 	}
-
-
 
 	//
 	// 4. lépés: lépjünk ki
---
+++ 03_SDLOGL_FW/03_SDLOGL_FW/MyApp.cpp
@@ -0,0 +1,79 @@
+#include "MyApp.h"
+#include <math.h>
+
+CMyApp::CMyApp(void)
+{
+}
+
+
+CMyApp::~CMyApp(void)
+{
+}
+
+bool CMyApp::Init()
+{
+	// törlési szín legyen kékes
+	glClearColor(0.125f, 0.25f, 0.5f, 1.0f);
+
+	// kapcsoljuk be a hatrafele nezo lapok eldobasat
+	glEnable(GL_CULL_FACE);
+	glEnable(GL_DEPTH_TEST);
+
+	return true;
+}
+
+void CMyApp::Clean()
+{
+}
+
+void CMyApp::Update()
+{
+}
+
+
+void CMyApp::Render()
+{
+	// töröljük a frampuffert (GL_COLOR_BUFFER_BIT) és a mélységi Z puffert (GL_DEPTH_BUFFER_BIT)
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// törlési szín függjön a felhasznalo inputjatol
+	glClearColor(backgroundColor.r / 255.0, backgroundColor.g / 255.0, backgroundColor.b / 255.0, 1.0f);
+}
+
+void CMyApp::KeyboardDown(SDL_KeyboardEvent& key)
+{
+	// a "memoria" valtoztatasa
+	if (key.keysym.sym == SDLK_t) {
+		backgroundColor = { 0, 0, 255, 255 };
+	} else if (key.keysym.sym == SDLK_r) {
+		backgroundColor = { 0, 255, 0, 255 };
+	}
+}
+
+void CMyApp::KeyboardUp(SDL_KeyboardEvent& key)
+{
+}
+
+void CMyApp::MouseMove(SDL_MouseMotionEvent& mouse)
+{
+
+}
+
+void CMyApp::MouseDown(SDL_MouseButtonEvent& mouse)
+{
+
+}
+
+void CMyApp::MouseUp(SDL_MouseButtonEvent& mouse)
+{
+}
+
+void CMyApp::MouseWheel(SDL_MouseWheelEvent& wheel)
+{
+}
+
+// a két paraméterbe az új ablakméret szélessége (_w) és magassága (_h) található
+void CMyApp::Resize(int _w, int _h)
+{
+	glViewport(0, 0, _w, _h );
+}
